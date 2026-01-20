38d19e1b2078075d4df40eca40faead10d0312ea
GUONING LYU <lgn21st@gmail.com>
2026-01-20 08:03:36 +0800

    Expand rotation kick tests

--- tests/rotation_kicks.rs
+++ tests/rotation_kicks.rs
@@ -24,3 +24,34 @@ fn rotate_kicks_inside_right_wall() {
     state.apply_action(GameAction::RotateCw);
     assert!(state.active.x <= BOARD_WIDTH as i32 - 1);
 }
+
+#[test]
+fn rotate_kicks_around_blocker() {
+    let mut state = GameState::new(3, GameConfig::default());
+    state.board = Board::new();
+    state.active = Tetromino::new(TetrominoType::T, 0, 0);
+    state.active.rotation = Rotation::North;
+
+    state.board.cells[0][1].filled = true;
+    state.board.cells[0][1].kind = Some(TetrominoType::I);
+
+    state.apply_action(GameAction::RotateCw);
+    assert_eq!(state.active.rotation, Rotation::East);
+    assert_ne!(state.active.x, 0);
+}
+
+#[test]
+fn rotate_fails_when_all_kicks_blocked() {
+    let mut state = GameState::new(4, GameConfig::default());
+    state.board = Board::new();
+    state.active = Tetromino::new(TetrominoType::T, 0, 0);
+    state.active.rotation = Rotation::North;
+
+    for (x, y) in [(0, 0), (1, 0), (2, 0), (3, 0)].iter() {
+        state.board.cells[*y][*x].filled = true;
+        state.board.cells[*y][*x].kind = Some(TetrominoType::O);
+    }
+
+    state.apply_action(GameAction::RotateCw);
+    assert_eq!(state.active.rotation, Rotation::North);
+}
